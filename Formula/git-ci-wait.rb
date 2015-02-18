32ed0c68d4d2f57c933b4dd6d5aec6f1247217be
Bob <bob@zoller.us>
2015-02-18 13:50:16 -0800

    initial commit

---
+++ Formula/git-ci-wait.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class GitCiWait < Formula
+  homepage 'https://github.com/bobzoller/homebrew-bobzoller/'
+  url 'https://github.com/bobzoller/homebrew-bobzoller.git'
+  version '1.0.0'
+
+  skip_clean 'bin'
+
+  def install
+    bin.install 'git-ci-wait'
+    (bin+'git-ci-wait').chmod 0755
+  end
+
+end
---
+++ README.md
@@ -0,0 +1,15 @@
+homebrew-bobzoller
+------------------
+Bob's personal Homebrew formulae.
+
+* `git-ci-wait` -- blocking wrapper around `hub ci-status`
+
+----
+
+## Installation
+
+```
+brew tap bobzoller/homebrew-bobzoller
+brew install git-ci-wait
+```
+
