5bd09152a1f46611a51add1d28240c54a9fbe2ef
Bob <bob@zoller.us>
2015-03-11 15:30:54 -0700

    adds npm-bump

---
+++ Formula/npm-bump.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class NpmBumpWait < Formula
+  homepage 'https://github.com/bobzoller/homebrew-bobzoller/'
+  url 'https://github.com/bobzoller/homebrew-bobzoller.git'
+  version '1.0.0'
+
+  skip_clean 'bin'
+
+  def install
+    bin.install 'npm-bump'
+    (bin+'npm-bump').chmod 0755
+  end
+
+end
